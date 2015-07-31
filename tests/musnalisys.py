bbf8ce7baf6e23de80bfc3f7b87b6bcacdf9a6cc
zardoru <forsakengmex@gmail.com>
2015-07-31 16:09:57 -0300

    reorganize analisys file

--- tests/musnalisys.py
+++ tests/musnalisys.py
@@ -1,7 +1,6 @@
 __author__ = 'Agka'
 
 from osutk.storyboard import *
-from osutk.translate import *
 
 # File to open and do the analysis on. Note that python only has support for wav.
 input_file = "input.wav"
@@ -13,7 +12,7 @@ output_file = "fft.osb"
 widescreen = 1
 
 # How many frames per second to render the FFT.
-framerate = 20
+framerate = 30
 
 # Audio sampling rate. Adjust properly.
 sample_rate = 44100.0
@@ -25,13 +24,16 @@ max_scale = 0.8
 min_scale = 0.1
 
 # Horizontal scale
-h_scale = 0.4
+h_scale = 0.6
 
-# When to fade out (in ms) the objects.
-fadeout_start = from_osu_time_notation("01:42:220 - ")
+# Origin of the bars
+bar_origin = Origin.BottomCenter
+
+# Location of the bars (y axis
+bar_loc = Screen.Height
 
 # For how long to fade out
-fade_threshold = 5000  # 5 sec
+fade_threshold = 500  # 0.5 sec
 
 # Bar filename
 bar_fn = "bar.png"
@@ -39,29 +41,31 @@ bar_fn = "bar.png"
 ## Advanced
 
 # Whether to use the hann window on the samples
-use_window = 0
+use_window = 1
 
 # In the strange case your mp3 and your wav differ, adjust this. Otherwise leave as-is.
 offset = 0
 
 # repeat fft twice instead of reducing it
-symmetrical = 1
+symmetrical = 0
 
 # use center as low frequencies and edges as high if symmetrical
 sym_reverse = 1
 
 # INTERNALS
 ##############################################################################################
-import numpy
+from time import time
 import math
 import wave
 from numpy.fft import fft, fftfreq
 from struct import unpack
 
+process_time = time()
+
 # Pick the octave thirds centers to draw. One object per requency. Adjust accordingly with framerate.
-octavecenters = [31.5, 40, 50, 63, 80, 100, 125,
+octavecenters = [50, 80, 100, 125,
                  160, 200, 315, 400, 500, 630, 800, 1000, 1250,
-                 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000]
+                 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000]
 
 if symmetrical:
     if sym_reverse:
@@ -82,7 +86,6 @@ else:
 samples_per_frame = int(sample_rate / framerate)  # audio frames per video frame
 
 # Music precalculations.
-
 print("{} sub-octave centers".format(objects))
 
 # Calculate upper and lower bounds of every octave.
@@ -90,8 +93,8 @@ base_round = lambda val, base: int(base * round(float(val)/base))
 fft_frequencies = [int(x) for x in fftfreq(samples_per_frame, 1.0 / sample_rate)]
 difference = fft_frequencies[1] - fft_frequencies[0]
 
-upper_octaves = [base_round(x * 2.0 ** (1.0 / (2.0 ** (1 / (2 * 3)))), difference) for x in octavecenters]
-lower_octaves = [base_round(x / 2.0 ** (1.0 / (2.0 ** (1 / (2 * 3)))), difference) for x in octavecenters]
+upper_octaves = [base_round(x * (2.0 ** (1.0 / (2 * 3))), difference) for x in octavecenters]
+lower_octaves = [base_round(x / (2.0 ** (1.0 / (2 * 3))), difference) for x in octavecenters]
 
 print("Octave bounds: \n\tLow: {} \n\tHigh: {}".format(lower_octaves, upper_octaves))
 
@@ -101,19 +104,96 @@ lower_index = [fft_frequencies.index(x) if x in fft_frequencies else len(fft_fre
 
 print("Octave indices: \n\tLow: {} \n\tHigh: {}".format(lower_index, upper_index))
 
-# SB output. Adjust this if you'd rather have different parameters
+# FFT and Audio Processing
 ###############################################################################
-x_offset = 0
-if widescreen:
-    x_offset = Screen.StartWidescreen
 
-sprites = [Sprite(file=bar_fn, location=[x*size + size / 2 + x_offset, Screen.Height], origin=Origin.BottomCenter)
-           for x in range(objects)]
+def average(bts):
+    # separate interleaved s16 data and normalize
+    bts = unpack("%ih" % (len(bts) / 2), bytes(bts))
+    left_ch = [v * divisor for v in bts[::2]]
+    right_ch = [v * divisor for v in bts[1::2]]
 
-# Fade out. Recommended that you adjust the parameters instead.
-for x in sprites:
-    x.fade(Ease.Linear, 0, fadeout_start, 1, 1)
-    x.fade(Ease.Linear, fadeout_start, fadeout_start + fade_threshold, 1, 0)
+    avg_amplitude = []
+    for u in range(len(left_ch)):
+        # average both channels into -1.0 <-> 1.0 space
+        avg_amplitude.append((left_ch[u] + right_ch[u]) / 2)
+
+    return avg_amplitude
+
+def apply_hanning_window(wnd):
+    for i in range(len(wnd)):
+        wnd[i] *= 0.5 - 0.5 * math.cos(2 * math.pi * i / (len(wnd) - 1))
+
+def do_fft():
+    # assumptions: input.wav is stereo int16 data @ 44100Hz.
+    with wave.open(input_file) as wav:
+        global fadeout_start
+        duration = wav.getnframes() / wav.getframerate()
+        frames_total = wav.getnframes()
+        fadeout_start = round(duration * 1000)
+        print("Audio lasts for {} frames. {} audio frames per video frame".format(frames_total, samples_per_frame))
+        print("Total time: {}".format(duration))
+        print("Channels: {}, Bytes/Sample: {} Framerate: {}".format(wav.getnchannels(),
+                                                                    wav.getsampwidth(),
+                                                                    wav.getframerate()))
+
+        freq_center = len(fft_frequencies) // 2
+        print("Frequencies: {} len: {}, max: {}".format(fft_frequencies[:freq_center], freq_center, max(fft_frequencies)))
+
+        current_frame = 0
+        while current_frame < frames_total:
+            frames = wav.readframes(samples_per_frame)
+            frames = average(frames)
+            if use_window:
+                apply_hanning_window(frames)
+
+            if len(frames) == 0:
+                print("read 0 frames D:")
+                break
+            fft_values = fft(frames)
+            fft_values = abs(fft_values)
+
+            unique_pts = len(fft_values) / 2
+            l, r = fft_values[:unique_pts], fft_values[unique_pts:]
+            if not symmetrical:  # use both sides of FFT
+                fft_values = l
+            else:
+                if sym_reverse:
+                    fft_values = l[::-1] + l
+                else:
+                    fft_values = l + l[::-1]
+
+            bins_this_frame = []
+            for bin in range(objects):
+                bin_max = 0
+                # get average power of bin. first get the range of frequencies for this bin
+                range_len = upper_index[bin] - lower_index[bin]
+                if range_len != 0:
+                    for frequency_value in range(lower_index[bin], upper_index[bin]):
+                        if frequency_value < len(fft_values):
+                            bin_max = max(10 * math.log10(fft_values[frequency_value] ** 2), bin_max)
+                    # There's several ways of doing this; raw amplitude, power, decibel scale
+                    # you could take the average or the maximum, I settle for the max.
+
+                    max_power[bin] = max(max_power[bin], bin_max)
+                    min_power[bin] = min(min_power[bin], bin_max)
+
+                    # add to set of bins this audio frame
+                    bins_this_frame.append(bin_max)
+                else:
+                    raise ValueError("The framerate is too high for the frequency {} to be properly represented"\
+                                     .format(octavecenters[bin]))
+
+            # output to storyboard
+            # add bins to fft analysis data
+            frame_fft.append(((current_frame - samples_per_frame) * 1000 / wav.getframerate(),
+                              current_frame * 1000 / wav.getframerate(), bins_this_frame))
+            current_frame += samples_per_frame
+            print(" Processed: {:.0f} second(s)".format(current_frame / wav.getframerate()), end='\r')
+
+
+# Final output.
+###############################################################################
 
 prev_fft = [0 for x in range(objects)]
 # FFT scaling. Don't touch this.
@@ -136,99 +216,27 @@ def put_fft(fft_tuples, start, end):
 
     prev_fft = fft_tuples
 
-# FFT and Audio Processing
-###############################################################################
-
-def average(bts):
-    # separate interleaved data and normalize
-    bts = unpack("%ih" % (len(bts) / 2), bts)
-    left_ch = [v * divisor for v in bts[::2]]
-    right_ch = [v * divisor for v in bts[1::2]]
-
-    rt = []
-    for u in range(len(left_ch)):
-        # average both channels into -1.0 <-> 1.0 space
-        rt.append((left_ch[u] + right_ch[u]) / 2)
-
-    return rt
-
-# assumptions: input.wav is stereo int16 data @ 44100Hz.
-with wave.open(input_file) as wav:
-    duration = wav.getnframes()
-    print("Audio lasts for {} frames. {} audio frames per video frame".format(duration, samples_per_frame))
-    print("Total time: {}".format(duration / wav.getframerate()))
-    print("Channels: {}, Bytes/Sample: {} Framerate: {}".format(wav.getnchannels(),
-                                                                wav.getsampwidth(),
-                                                                wav.getframerate()))
-
-    freq_center = len(fft_frequencies) // 2
-    print("Frequencies: {} len: {}, max: {}".format(fft_frequencies[:freq_center], freq_center, max(fft_frequencies)))
-    binlen = int(samples_per_frame / objects)
-    print("avg bin size: {}".format(binlen))
-
-    x = 0
-    while x < duration:
-        frames = wav.readframes(samples_per_frame)
-        frames = average(frames)
-
-        if use_window:
-            for i, x in enumerate(numpy.hanning(len(frames))):
-                frames[i] *= x
-
-        if len(frames) == 0:
-            print("read 0 frames D:")
-            break
-        fft_values = fft(frames)
-        fft_values = abs(fft_values)
-
-        unique_pts = len(fft_values) / 2
-        l, r = fft_values[:unique_pts], fft_values[unique_pts:]
-        if not symmetrical:  # use both sides of FFT
-            fft_values = l
-        else:
-            if sym_reverse:
-                fft_values = l[::-1] + l
-            else:
-                fft_values = l + l[::-1]
-
-        octs = []
-        for n in range(objects):
-            binavg = 0
-            # get average power of bin. first get the range of frequencies for this bin
-            range_len = upper_index[n] - lower_index[n]
-            if range_len != 0:
-                for binv in range(lower_index[n], upper_index[n]):
-                    if binv < len(fft_values):
-                        binavg = max(10 * math.log10(fft_values[binv] ** 2), binavg)
-                # There's several ways of doing this; raw amplitude, power, decibel scale
-                # you could take the average or the maximum, I settle for the max.
-                # binavg /= float(range_len)
-
-                max_power[n] = max(max_power[n], binavg)
-                min_power[n] = min(min_power[n], binavg)
-
-                # add to set of bins this audio frame
-                octs.append(binavg)
-            else:
-                raise ValueError("The framerate is too high for the frequency {} to be properly represented"\
-                                 .format(octavecenters[n]))
-
-        # output to storyboard
-        # add bins to fft analysis data
-        frame_fft.append(((x - samples_per_frame) * 1000 / wav.getframerate(),
-                          x * 1000 / wav.getframerate(), octs))
-        x += samples_per_frame
-        print(" Processed: {:.0f} second(s)".format(x / wav.getframerate()), end='\r')
-
-print("\n")
-
-# Final output.
-###############################################################################
+do_fft()
+print("")
 print("Power: \n\tMin: {} \n\tMax: {}".format(min_power, max_power))
 
 print("Generating events.")
+
+x_offset = 0
+if widescreen:
+    x_offset = Screen.StartWidescreen
+
+sprites = [Sprite(file=bar_fn, location=[x*size + size / 2 + x_offset, bar_loc], origin=bar_origin)
+           for x in range(objects)]
+
+# Fade out. Recommended that you adjust the parameters instead.
+for x in sprites:
+    x.fade(Ease.Linear, 0, fadeout_start, 1, 1)
+    x.fade(Ease.Linear, fadeout_start, fadeout_start + fade_threshold, 1, 0)
 for x in frame_fft:
     put_fft(x[2], x[0], x[1])
 
 print("Exporting osb.")
 Storyboard.export("fft.osb")
+
+print("Processing took {:.2f} seconds.".format(time() - process_time))
